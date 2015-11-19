7fbea37a2e2ddf2ce5f8f5780142058ccf3dbe28
Cristina Botez <cristina.botez@3pillarglobal.com>
2015-11-19 17:37:52 +0200

    Added tests for M3U8.add_media modification.

--- tests/test_variant_m3u8.py
+++ tests/test_variant_m3u8.py
@@ -5,6 +5,7 @@
 
 import m3u8
 
+
 def test_create_a_variant_m3u8_with_two_playlists():
     variant_m3u8 = m3u8.M3U8()
 
@@ -44,6 +45,7 @@ http://example.com/low.m3u8
 http://example.com/high.m3u8
 """
     assert expected_content == variant_m3u8.dumps()
+
 
 def test_create_a_variant_m3u8_with_two_playlists_and_two_iframe_playlists():
     variant_m3u8 = m3u8.M3U8()
@@ -134,8 +136,8 @@ def test_variant_playlist_with_average_bandwidth():
                      'average_bandwidth': 2857123,
                      'program_id': 1,
                      'subtitles': 'subs'},
-       media=[],
-       base_uri=None
+        media=[],
+        base_uri=None
     )
 
     variant_m3u8.add_playlist(low_playlist)
@@ -149,3 +151,65 @@ http://example.com/low.m3u8
 http://example.com/high.m3u8
 """
     assert expected_content == variant_m3u8.dumps()
+
+
+def test_playlist_stores_1st_of_2_equivalent_media_items():
+    variant_m3u8 = m3u8.M3U8()
+    default_subs = m3u8.Media(
+        group_id='subs',
+        type='SUBTITLES',
+        name='English',
+        uri='english_sub.m3u8',
+        language='en',
+        default='YES',
+        autoselect='YES',
+        forced='NO'
+    )
+    non_default_subs = m3u8.Media(
+        group_id='subs',
+        type='SUBTITLES',
+        name='English',
+        uri='english_sub.m3u8',
+        language='en',
+        default='NO',
+        autoselect='NO',
+        forced='NO'
+    )
+    variant_m3u8.add_media(default_subs)
+
+    variant_m3u8.add_media(non_default_subs)
+
+    assert len(variant_m3u8.media) == 1
+    collected_subs = variant_m3u8.media.pop()
+    assert collected_subs.default == collected_subs.autoselect == 'YES'
+
+
+def test_playlist_stores_2nd_of_2_equivalent_media_items_when_specified():
+    variant_m3u8 = m3u8.M3U8()
+    default_subs = m3u8.Media(
+        group_id='subs',
+        type='SUBTITLES',
+        name='English',
+        uri='english_sub.m3u8',
+        language='en',
+        default='YES',
+        autoselect='YES',
+        forced='NO'
+    )
+    non_default_subs = m3u8.Media(
+        group_id='subs',
+        type='SUBTITLES',
+        name='English',
+        uri='english_sub.m3u8',
+        language='en',
+        default='NO',
+        autoselect='NO',
+        forced='NO'
+    )
+    variant_m3u8.add_media(default_subs)
+
+    variant_m3u8.add_media(non_default_subs, replace=True)
+
+    assert len(variant_m3u8.media) == 1
+    collected_subs = variant_m3u8.media.pop()
+    assert collected_subs.default == collected_subs.autoselect == 'NO'
